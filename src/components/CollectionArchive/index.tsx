8f137fc6ae5a22201090b7279deeace0ed7c54ce
David Bradford <davidbradforduko@yahoo.com>
2025-09-26 11:27:48 +0100

    card data issue fixed

--- src/components/CollectionArchive/index.tsx
+++ src/components/CollectionArchive/index.tsx
@@ -1,20 +1,20 @@
 import { cn } from '@/utilities/ui'
 import React from 'react'
 
-import { Card, CardPostData } from '@/components/Card'
+// import { Card, CardPostData } from '@/components/Card'
 
-export type Props = {
-  posts: CardPostData[]
-}
+// export type Props = {
+//   posts: CardPostData[]
+// }
 
-export const CollectionArchive: React.FC<Props> = (props) => {
-  const { posts } = props
+export const CollectionArchive: React.FC = () => {
+  // const { posts } = props
 
   return (
     <div className={cn('container')}>
       <div>
         <div className="grid grid-cols-4 sm:grid-cols-8 lg:grid-cols-12 gap-y-4 gap-x-4 lg:gap-y-8 lg:gap-x-8 xl:gap-x-8">
-          {posts?.map((result, index) => {
+          {/* {posts?.map((result, index) => {
             if (typeof result === 'object' && result !== null) {
               return (
                 <div className="col-span-4" key={index}>
@@ -24,7 +24,7 @@ export const CollectionArchive: React.FC<Props> = (props) => {
             }
 
             return null
-          })}
+          })} */}
         </div>
       </div>
     </div>
