f363c351fe6caa1545ac0a0b541e552968612bae
David Bradford <davidbradforduko@yahoo.com>
2025-09-26 11:05:23 +0100

    card block fixed

--- src/blocks/RelatedPosts/Component.tsx
+++ src/blocks/RelatedPosts/Component.tsx
@@ -21,11 +21,11 @@ export const RelatedPosts: React.FC<RelatedPostsProps> = (props) => {
       {introContent && <RichText data={introContent} enableGutter={false} />}
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 items-stretch">
-        {docs?.map((doc, index) => {
+        {/* {docs?.map((doc, index) => {
           if (typeof doc === 'string') return null
 
           return <Card key={index} doc={doc} relationTo="posts" showCategories />
-        })}
+        })} */}
       </div>
     </div>
   )
