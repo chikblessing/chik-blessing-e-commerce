a813df8318d672e6191b435c3a8aede6dec53c25
David Bradford <davidbradforduko@yahoo.com>
2025-11-05 12:38:19 +0100

    fix order type

--- src/collections/Orders.ts
+++ src/collections/Orders.ts
@@ -602,7 +602,7 @@ export const Orders: CollectionConfig = {
               await req.payload.update({
                 collection: 'users',
                 id: customerId,
-                data: { orderHistory } as unknown,
+                data: { orderHistory } as any,
               })
             }
           } catch (error) {
